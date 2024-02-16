f5e9ca576c6154e729f64992eb78f3dc7f1bdf93
waimran <159434788+waimran@users.noreply.github.com>
2024-02-16 20:14:09 +0500

    Add files via upload

---
+++ q3.ts
@@ -0,0 +1,6 @@
+//name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let firstName: string = "wajiha imran";
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.charAt(0).toUpperCase() + firstname.slice(1));
