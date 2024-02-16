06a5c049c895d96b0a9b587385db10271dc608d4
waimran <159434788+waimran@users.noreply.github.com>
2024-02-16 19:48:24 +0500

    Add files via upload

---
+++ q5.js
@@ -0,0 +1,5 @@
+//3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var fName = 'Wajiha Imran';
+console.log("LowerCase: " + fName.toLowerCase());
+console.log("UpperCase: " + fName.toUpperCase());
+console.log("TitleCase: " + fName.charAt(0).toUpperCase() + fName.slice(1).toLowerCase());
